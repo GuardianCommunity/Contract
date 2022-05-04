f08282f796926b1509cdfa0333f8abfac4d3bfca
Ali Khazaee <dev.khazaee@gmail.com>
2022-05-04 18:42:18 +0430

    Update

--- roulette/Roulette.sol
+++ roulette/Roulette.sol
@@ -5,101 +5,140 @@ pragma solidity ^0.8.10;
 import "../interface/IBEP20.sol";
 
 /*
-    Error: 1 Roulette is not enable
-    Error: 2 Roulette has ended
+    Error: 1, Roulette is not active
+    Error: 2, Maximum participate reached
+    Error: 3, time
+    Error: 4, need to ends
+    Error: 2, Maximum participate reached
 */
 
-contract Roulette
+contract LuckyDraw
 {
-    IBEP20 private constant TOKEN = IBEP20(address(0));
+    IBEP20 private immutable TOKEN;
+    uint256 private immutable FEE;
+    uint256 private immutable TIME;
+    uint256 private immutable PRICE;
+    uint256 private immutable CHANCE;
+    
+    constructor(uint256 fee, uint256 time, uint256 price, uint256 chance, address token)
+    {
+        FEE = fee;
+        TIME = time;
+        PRICE = price;
+        CHANCE = chance;
+        TOKEN = IBEP20(token);
+    }
 
-    uint256 private immutable Time;
-    uint256 private immutable Price;
-    uint256 private immutable Chance;
+    struct Reward
+    {
+        bool Won;
+        bool Claim;
+        address Owner;
+    }
 
     uint256 private Hash;
-    address[] private Storage;
-
-    constructor()
-    {
-        Chance = 10;
-        Price = 1000;
-        Time = block.timestamp + 24 hours;
-    }
+    uint256 private Nonce;
+    mapping(uint256 => Reward) private TicketMap;
+    mapping(address => uint256[]) private ParticipatetMap;
 
     function Join() external
     {
         require(Hash == 0, "1");
 
-        TOKEN.transferFrom(msg.sender, address(this), Price);
+        require(ParticipatetMap[msg.sender].length < 10, "2");
 
-        Storage.push(msg.sender);
+        TOKEN.transferFrom(msg.sender, address(this), PRICE);
+
+        ParticipatetMap[msg.sender].push(Nonce);
+
+        TicketMap[Nonce].Owner = msg.sender;
+
+        Nonce++;
     }
 
     function Finish() external
     {
         require(Hash == 0, "1");
 
-        require(block.timestamp > Time, "3");
+        require(block.timestamp > TIME, "3");
 
         Hash = uint256(keccak256(abi.encodePacked(block.difficulty, address(this), block.timestamp, block.number, block.basefee)));
+
+        uint256 WinnerCount = Nonce / CHANCE;
+
+        for (uint256 I = 0; I < WinnerCount; I++)
+        {
+            Hash = uint256(keccak256(abi.encodePacked(Hash, I)));
+
+            TicketMap[Hash % Nonce].Won = true;
+        }
     }
 
-    function GetWinner(address Winner) external view returns (uint256[] memory Result)
+    function Claim() external
     {
-        address[] memory StorageTemp = Storage;
+        require(Hash != 0, "4");
 
-        for (uint256 I = 0; I < StorageTemp.length; I++)
+        uint256 Amount;
+
+        for (uint256 I = 0; I < ParticipatetMap[msg.sender].length; I++)
         {
-            uint256 N = I + Hash % (StorageTemp.length - I);
-            address T = StorageTemp[N];
+            uint256 NonceID = ParticipatetMap[msg.sender][I];
 
-            StorageTemp[N] = StorageTemp[I];
-            StorageTemp[I] = T;
-        }
-
-        uint256 J;
-        uint256 Max = (Chance > StorageTemp.length) ? StorageTemp.length : Chance;
-
-        for (uint256 I = 0; I < Max; I++)
-        {
-            if (StorageTemp[I] == Winner)
+            if (TicketMap[NonceID].Won && !TicketMap[NonceID].Claim)
             {
-                Result[J] = I;
+                TicketMap[NonceID].Claim = true;
 
-                J++;
+                Amount += PRICE;
             }
         }
+
+        if (Amount > 0)
+        {
+            Amount = Amount / FEE;
+
+            TOKEN.transfer(msg.sender, Amount);
+        }
     }
 
-    function GetStorage() external view returns (address[] memory)
+    function Claimable() external view returns (uint256)
     {
-        return Storage;
+        uint256 Amount;
+
+        for (uint256 I = 0; I < ParticipatetMap[msg.sender].length; I++)
+        {
+            uint256 NonceID = ParticipatetMap[msg.sender][I];
+
+            if (TicketMap[NonceID].Won && !TicketMap[NonceID].Claim)
+            {
+                Amount += PRICE;
+            }
+        }
+
+        return Amount;
+    }
+
+    function GetTicketCount(address Account) external view returns (uint256)
+    {
+        return ParticipatetMap[Account].length;
+    }
+
+    function GetReward() external view returns (uint256)
+    {
+        return (PRICE * Nonce) / FEE;
     }
 
     function GetPrice() external view returns (uint256)
     {
-        return Price;
+        return PRICE;
     }
 
     function GetChance() external view returns (uint256)
     {
-        return Chance;
+        return CHANCE;
     }
 
     function GetTime() external view returns (uint256)
     {
-        return Time;
+        return TIME;
     }
 }
-
-/*
-    for (uint256 I = 0; I < Storage.length; I++)
-    {
-        uint256 N = I + Hash % (Storage.length - I);
-        address T = Storage[N];
-
-        Storage[N] = Storage[I];
-        Storage[I] = T;
-    }
-*/
